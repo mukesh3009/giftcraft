d4392dc5379d720c58c2da0a0e10bc8ed898ade0
Mukesh <mukeshpalanikumar@gmail.com>
2026-04-19 02:01:53 +0530

    fix app initialization

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const cors = require('cors');
 const multer = require('multer');
 require('dotenv').config();
 
-const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
