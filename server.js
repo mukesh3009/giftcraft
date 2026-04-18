200fbff2803d4fe057f694042ecb56cc5a454c3e
Mukesh <mukeshpalanikumar@gmail.com>
2026-04-19 01:59:01 +0530

    fix app initialization

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.static(__dirname));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
